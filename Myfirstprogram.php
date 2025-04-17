ec8332e9d496ef12e56bb2c03b00ac20d29cbf71
DivyaMuruganIBM <110721598+DivyaMuruganIBM@users.noreply.github.com>
2025-04-17 13:11:31 +0530

    Add files via upload

---
+++ Myfirstprogram.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <p>This is my first PHP program using XAMPP.</p>
+</body>
+</html>
---
+++ Phpcode.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>My Page</title>
+</head>
+<body>
+    <h1>PHP Page</h1>
+    <?php
+    echo "<font color='red'>Divya</font><br>";                       print "<font color='blue'>Divya</font><br>";  
+    echo "<font color='green'>Divya</font><br>"; 
+    echo "<font color='purple'>Divya</font><br>";
+    echo "<font color='orange'>Divya</font><br>";
+    echo "Divya<br>"; 
+    echo "Sowndarya<br>";
+    echo "Shobana<br>";
+    $var = print "Its good<br>";  
+    //echo $var;  
+    ?>
+</body>
+</html>
